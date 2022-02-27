a7196f03c6f2cebc107481a18d7d896a7e6fdf7e
maciekp9 <maciek.mp15@gmail.com>
2022-02-27 15:22:15 +0100

    Created new project with tab bar navi

--- MyWeather/Controller/SettingsController.swift
+++ MyWeather/Controller/SettingsController.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import UIKit
+
+class SettingsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
