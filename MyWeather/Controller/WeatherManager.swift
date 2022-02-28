6eab3f5c6177e349a0e3db34c089ff5c1ccf71bf
maciekp9 <maciek.mp15@gmail.com>
2022-02-28 22:37:31 +0100

    test gitignore

---
+++ MyWeather/Controller/WeatherManager.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherManager.swift
+//  MyWeather
+//
+//  Created by Maciej Przydatek on 28/02/2022.
+//
+
+import Foundation
+import CoreLocation
+
+struct WeatherManager {
+    
+    
+    let baseURL = "https://api.openweathermap.org/data/2.5/weather?"
+    
+    
+    
+    
+}
